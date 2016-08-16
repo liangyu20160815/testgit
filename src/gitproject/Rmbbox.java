ccca86deaaddba5af41cac6862250f2f7edf4531
liangyu20160815 <546773086@qq.com>
2016-08-16 09:41:12 +0800

    新增文件

---
+++ src/gitproject/Rmbbox.java
@@ -0,0 +1,9 @@
+package gitproject;
+
+public class Rmbbox {
+
+	public static void main(String[] args) {
+		System.out.println("增加一个新文件");
+	}
+
+}
---
+++ src/gitproject/Test.java
@@ -0,0 +1,9 @@
+package gitproject;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("第一个git项目");
+	}
+
+}
