e58a2be1f006c842cd0e3e5a38fb973d162ac7db
liangyu20160815 <546773086@qq.com>
2016-08-18 17:50:32 +0800

    ec

--- src/gitproject/Test2.java
+++ src/gitproject/Test2.java
@@ -5,12 +5,6 @@ public class Test2 {
 	public static void main(String[] args) {
 		System.out.println("111");
 		System.out.print("idea add");
-		System.out.println("");
-		System.out.print("idea second");
-		System.out.println("eclipse 2222");
-		System.out.print("idea 1111");System.out.print("idea second");
-		System.out.println("eclipse 2222");
-		System.out.print("idea 1111");
 	}
 
 }
