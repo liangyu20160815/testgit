ab34d96dc8d942bd89be7aded1f2e24a92875528
liangyu20160815 <546773086@qq.com>
2016-08-18 18:22:08 +0800

    eclipse s

--- src/gitproject/Test2.java
+++ src/gitproject/Test2.java
@@ -4,9 +4,6 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		System.out.println("111");
-		System.out.print("idea add");
-		System.out.print("idea modify");
-		System.out.print("idea delete");
 		System.out.println("eclipse modify");
 	}
 
